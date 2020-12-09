4eb533a1e91117a3c08049f955c7daa9c0f2bde9
Owen Edwards <owen.r.edwards@gmail.com>
2020-12-08 17:58:53 -0800

    Make the cleaned-up HTML region select-all on focus.

--- Source/common.js
+++ Source/common.js
@@ -10,3 +10,14 @@ Array.from(inputTexts).forEach((inputText) => {
         inputText.select();
     });
 });
+var editableCodeAreas = document.querySelectorAll("code[contenteditable=true]");
+Array.from(editableCodeAreas).forEach((editableCodeArea) => {
+    editableCodeArea.addEventListener("focus", (ev) => {
+        // See https://stackoverflow.com/questions/6139107/programmatically-select-text-in-a-contenteditable-html-element/6150060#6150060
+        var range = document.createRange();
+        range.selectNodeContents(editableCodeArea);
+        var sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+    });
+});
