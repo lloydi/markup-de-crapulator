2d29d4d7c51977dc8729120e24feb9c5edf8f8df
lloydi <github@lloydi.com>
2021-05-12 22:50:41 +0100

    Fixed text selection bug

--- Source/common.js
+++ Source/common.js
@@ -1,23 +1,20 @@
-var textareas = document.querySelectorAll("textarea");
+function selectElementContents(el) {
+    var range = document.createRange();
+    range.selectNodeContents(el);
+    var sel = window.getSelection();
+    sel.removeAllRanges();
+    sel.addRange(range);
+}
+
+var textareas = document.querySelectorAll("textarea,code[contenteditable]");
 Array.from(textareas).forEach((textarea) => {
     textarea.addEventListener("focus", (ev) => {
-        textarea.select();
+        selectElementContents(textarea);
     });
 });
 var inputTexts = document.querySelectorAll("input[type=text]");
 Array.from(inputTexts).forEach((inputText) => {
     inputText.addEventListener("focus", (ev) => {
         inputText.select();
-    });
-});
-var editableCodeAreas = document.querySelectorAll("code[contenteditable=true]");
-Array.from(editableCodeAreas).forEach((editableCodeArea) => {
-    editableCodeArea.addEventListener("focus", (ev) => {
-        // See https://stackoverflow.com/questions/6139107/programmatically-select-text-in-a-contenteditable-html-element/6150060#6150060
-        var range = document.createRange();
-        range.selectNodeContents(editableCodeArea);
-        var sel = window.getSelection();
-        sel.removeAllRanges();
-        sel.addRange(range);
     });
 });
